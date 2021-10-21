ca7c6dbc6763237fae58b63cc57f7fea53bdf039
lilei <lilei@lileideMacBook-Air.local>
2021-10-21 16:42:48 +0800

    完成所有功能

--- app/src/main/java/com/sunnyweather/android/logic/Repository.kt
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -1,10 +1,15 @@
 package com.sunnyweather.android.logic
 
 import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.dao.PlaceDao
 import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.network.SunnyWeatherNetWork
 import kotlinx.coroutines.Dispatchers
-import java.lang.Exception
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.Exception
+
 
 
 object Repository {
@@ -23,4 +28,47 @@ object Repository {
         emit(result)
 
     }
+
+    fun refreshWeather(lng :String, lat:String) = liveData(Dispatchers.IO){
+        val result = try{
+            coroutineScope {
+                val deferredRealTime = async {
+                    SunnyWeatherNetWork.getRealtimeWeather(lng,lat)
+                }
+                val deferredDaily = async{
+                    SunnyWeatherNetWork.getDailyWeather(lng,lat)
+                }
+                val realtimeResponse = deferredRealTime.await()
+                val dailyResponse = deferredDaily.await()
+                if(realtimeResponse.status == "ok" && dailyResponse.status=="ok"){
+                    val weather = Weather(realtimeResponse.result.realtime,dailyResponse.result.daily)
+                    Result.success(weather)
+                }else{
+                    Result.failure(
+                        RuntimeException("realtime response status is ${realtimeResponse.status}" +
+                        "daily response status is ${dailyResponse.status}")
+                    )
+                }
+            }
+
+        }catch(e:Exception){
+            Result.failure<Weather>(e)
+        }
+        emit(result)
+    }
+    /*
+    private fun <T> fire(context: CoroutineContext,block:suspend()->Result<T>) =
+        liveData<Result<T>>(context){
+        val result = try{
+            block()
+        }catch(e:Exception){
+            Result.failure<T>(e)
+        }
+            emit(result)
+    }
+     */
+
+    fun savePlace(place : Place) = PlaceDao.savePace(place)
+    fun getPlace() : Place = PlaceDao.getSavedPlace()
+    fun isPlaceSaved() = PlaceDao.isPlaceSaved()
 }
--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -13,4 +13,8 @@ class PlaceViewModel : ViewModel() {
     fun searchPlaces(query : String){
         searchLiveData.value = query
     }
+
+    fun savePlace(place:Place) = Repository.savePlace(place)
+    fun getSavedPlace() : Place = Repository.getPlace()
+    fun isPlaceSaved() = Repository.isPlaceSaved()
 }
